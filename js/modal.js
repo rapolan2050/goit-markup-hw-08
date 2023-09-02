6c40ec8776ba5382e18fc87d8e5ddff4baff4814
Raul Camacho <elaragat2050@gmail.com>
2023-09-02 09:46:45 -0500

    sass structure running all

--- js/modal.js
+++ js/modal.js
@@ -1,24 +1,23 @@
 (() => {
 
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
-  };
+    const refs = {
+        openModalBtn: document.querySelector("[data-modal-open]"),
+        closeModalBtn: document.querySelector("[data-modal-close]"),
+        modal: document.querySelector("[data-modal]"),
+    };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-  }
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+    }
 })();
 
-function mostrar(){
+function mostrar() {
     document.getElementById('check-svg').style.display = 'block';
-    }
-  
-function ocultar(){
-	document.getElementById('check-svg').style.display = 'none';
-	}
+}
 
+function ocultar() {
+    document.getElementById('check-svg').style.display = 'none';
+}
